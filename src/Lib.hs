5790e020dda2e38c2483ab33956e1475344185d3
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2019-02-16 18:53:13 -0800

    running python in haskell, still trying to reflect json string into haskell type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,42 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( someFunc, pythonSrc, runHello, MyType
     ) where
+
+import qualified Data.ByteString.Lazy as BL
+import Data.Aeson
+import Data.Conduit
+import Data.Conduit.Shell
+import GHC.Generics
+
+import Debug.Trace
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+pythonSrc :: FilePath
+pythonSrc = "./src/hello.py"
+
+myConduit = do
+  Just x <- await
+  traceM . show $ x
+  yield x
+
+runHello = run (
+  python pythonSrc $|
+  conduit myConduit
+  )
+-- runHello = run (
+--   python pythonSrc $|
+-- )
+
+-- decodeConduit :: ConduitT BL.ByteString BL.ByteString IO (Maybe MyType)
+-- decodeConduit = 
+
+data MyType = MyType {foo :: Int} deriving (Show, Generic)
+instance FromJSON MyType
+instance ToJSON MyType
+
+mine = MyType 2
