f891eaff0a24e700dd99b13e0e696e4fa935f9dd
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2019-02-16 22:54:34 -0800

    stripping newlines from bytestrings

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,13 +5,23 @@ module Lib
     ( someFunc, pythonSrc, runHello, MyType
     ) where
 
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as CH
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as CHL
 import Data.Aeson
 import Data.Conduit
 import Data.Conduit.Shell
+import Data.Maybe
 import GHC.Generics
 
 import Debug.Trace
+
+debug = traceM . show
+toWord = BS.head . CH.pack
+newline = toWord "\n"
+teststring = CH.pack "\nhello\nworld\n"
+stripNewline = BS.filter (/= newline)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -19,24 +29,21 @@ someFunc = putStrLn "someFunc"
 pythonSrc :: FilePath
 pythonSrc = "./src/hello.py"
 
-myConduit = do
-  Just x <- await
-  traceM . show $ x
+mine = MyType 2
+
+extractMyType = do
+  mx <- await
+  let x = fromMaybe BS.empty mx
+  -- let y = stripNewline x
+  debug x
+  -- debug y
   yield x
 
 runHello = run (
   python pythonSrc $|
-  conduit myConduit
+  conduit extractMyType
   )
--- runHello = run (
---   python pythonSrc $|
--- )
-
--- decodeConduit :: ConduitT BL.ByteString BL.ByteString IO (Maybe MyType)
--- decodeConduit = 
 
 data MyType = MyType {foo :: Int} deriving (Show, Generic)
 instance FromJSON MyType
 instance ToJSON MyType
-
-mine = MyType 2
